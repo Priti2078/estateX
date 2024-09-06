0048247d935d849a1e5e93560b6accf34af9d53c
Priti <kapoorpriti18@gmail.com>
2024-09-06 17:04:24 +0530

    update and delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,13 +8,17 @@ import {
 } from "firebase/storage";
 import { app } from "../firebase";
 import { useDispatch } from 'react-redux';
+
+import { updateUserStart,updateUserSuccess,updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from "../redux/user/userSlice";
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser} = useSelector((state) => state.user);
+  const { currentUser, loading, error} = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess]=useState(false);
+  const  dispatch = useDispatch();
 
   useEffect(() => {
     if (file) {
@@ -46,10 +50,54 @@ export default function Profile() {
     );
   };
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
+  const handleDeleteUser = async ()=>{
+    try{
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+        method:'DELETE',
+      });
+      const data= await res.json();
+      if(data.success === false){
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    }catch(error){
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type="file"
@@ -79,32 +127,31 @@ export default function Profile() {
         <input
           type="text"
           placeholder="username"
-          // defaultValue={currentUser.username}
+          defaultValue={currentUser.username}
           id="username"
           className="border p-3 rounded-lg"
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           id="email"
-          // defaultValue={currentUser.email}
+          defaultValue={currentUser.email}
           className="border p-3 rounded-lg"
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
-          // onChange={handleChange}
+          onChange={handleChange}
           id="password"
           className="border p-3 rounded-lg"
         />
         <button
-          // disabled={loading}
+          disabled={loading}
           className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
         >
-          update
-          {/* {loading ? "Loading..." : "Update"} */}
+          {loading ? "Loading..." : "Update"}
         </button>
         {/* <Link
           className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
@@ -115,7 +162,7 @@ export default function Profile() {
       </form>
       <div className="flex justify-between mt-5">
         <span
-          // onClick={handleDeleteUser}
+          onClick={handleDeleteUser}
           className="text-red-700 cursor-pointer"
         >
           Delete account
@@ -123,6 +170,58 @@ export default function Profile() {
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
 
+      <p className="text-red-700 mt-5">{error ? error : ""}</p>
+      <p className="text-green-700 mt-5">
+        {updateSuccess ? "User is updated successfully!" : ""}
+      </p>
+      {/* <button onClick={handleShowListings} className="text-green-700 w-full">
+        Show Listings
+      </button> */}
+      {/* <p className="text-red-700 mt-5">
+        {showListingsError ? "Error showing listings" : ""}
+      </p> */}
+
+      {/* {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => ( */}
+      {/* <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            > */}
+      {/* <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold  hover:underline truncate flex-1"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link> */}
+
+      {/* <div className="flex flex-col item-center">
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 uppercase">Edit</button>
+                </Link>
+              </div> */}
+      {/* </div> */}
+      {/* ))} */}
+      {/* </div>
+      )}
+
+    </div> */}
     </div>
   );
 }
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -13,27 +13,57 @@ const userSlice = createSlice({
     signInStart: (state) => {
       state.loading = true;
     },
-    signInSuccess:(state,action)=>{
-        state.currentUser = action.payload;
-        state.loading = false;
-        state.error= null;
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
     },
-    signInFailure: (state,action)=>{
-        state.error = action.payload;
-        state.loading = false;
-    }
+    signInFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    updateUserStart: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    deleteUserStart: (state) =>{
+      state.loading = false;
+    },
+    deleteUserSuccess: (state)=>{
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure:(state,action)=>{
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 
-export const {signInStart, signInSuccess , signInFailure} = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+} = userSlice.actions;
 
 export default userSlice.reducer;
 
 export const { reducer: userReducer } = userSlice;
-
-
-
-
 
 // import { createSlice } from '@reduxjs/toolkit';
 
